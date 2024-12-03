383e5baeb84b65f17d0a20706dfac0c1e0400433
agiegnoj <jongg97@gmail.com>
2024-12-03 23:29:12 +0100

    .

--- sorting/SelectionSort.cpp
+++ sorting/SelectionSort.cpp
@@ -5,7 +5,7 @@ template <typename T>
 class SelectionSort{
     public:
 
-    vector <T> selectionSortAscending(vector<T>& arr){
+    vector <T> selectionSort(vector<T>& arr){
         static_assert(std::is_arithmetic<T>::value, "Type T must be arithmetic.");
 
         for (int i = 0; i<arr.size()-1; i++){
@@ -25,24 +25,6 @@ class SelectionSort{
         return arr;
     }
 
-    vector <T> insertionSortDescending(vector<T>& arr){
-
-        for (int i = 0; i<arr.size()-1; i++){
-            int maxIndex = i;
-            
-            for (int j = i+1; j< arr.size(); j++){
-                if (arr[j] > arr[maxIndex]){
-                    maxIndex = j;
-                }
-            }
-            
-            T temp = arr[i];
-            arr[i] = arr[maxIndex];
-            arr[maxIndex] = temp;
-        }
-
-        return arr;
-    }
 
 };
 
