a96d6ec19cc5b1239665f2251bb8ac7d8260018a
agiegnoj <jongg97@gmail.com>
2024-12-03 21:34:33 +0100

    .

--- sorting/InsertionSort.cpp
+++ sorting/InsertionSort.cpp
@@ -5,7 +5,7 @@ template <typename T>
 class InsertionSort{
     public:
 
-    vector <T> insertionSortAscending(vector<T>& arr){
+    vector <T> insertionSort(vector<T>& arr){
 
         static_assert(std::is_arithmetic<T>::value, "Type T must be arithmetic.");
 
@@ -15,23 +15,6 @@ class InsertionSort{
             T temp = arr[i];
 
             while (left >= 0  && temp < arr[left]){
-                arr[left+1] = arr [left];
-                arr [left] = temp;
-                left--;
-            }
-        }
-
-        return arr;
-    }
-
-    vector <T> insertionSortDescending(vector<T>& arr){
-
-        for (int i = 1; i < arr.size(); i++){
-           
-            int left = i-1;
-            T temp = arr[i];
-
-            while (left >= 0  && temp > arr[left]){
                 arr[left+1] = arr [left];
                 arr [left] = temp;
                 left--;
