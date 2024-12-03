2bba6395e7e3af1c730222d519e822ed35d1f547
agiegnoj <jongg97@gmail.com>
2024-12-03 18:44:59 +0100

    .

--- dynamic_programming/MaximumProductSubarray.cpp
+++ dynamic_programming/MaximumProductSubarray.cpp
@@ -9,13 +9,13 @@ class MaxProductSubarray{
 
         if (nums.size() == 0) return 0;
 
-        long currentMax = nums[0];
-        long currentMin = nums[0];
-        long globalMax = nums[0];
+        long long currentMax = nums[0];
+        long long currentMin = nums[0];
+        long long globalMax = nums[0];
 
         for (int num :nums){
-            long temp = max (max(currentMin * num, (long)num), currentMax*num);
-            currentMin = min(min(temp *num, (long)num), currentMin*num);
+            long long temp = max (max(currentMin * num, (long long)num), currentMax*num);
+            currentMin = min(min(temp *num, (long long)num), currentMin*num);
             currentMax = temp;
             globalMax = max (currentMax, globalMax);
         }       
